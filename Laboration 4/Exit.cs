3a3f89711d346484a6353e043b764bdc62291512
Khosro Jadidi <khosro.jadidi@gmail.com>
2019-09-30 00:17:25 +0200

    Created all classes, as well as interfaces, according to roadmap. All inheritances have been set. No actual code has been written as of yet.

---
+++ Laboration 4/Exit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_4
+{
+    abstract class Exit:AbstractMap,ILeaveable
+    {
+    }
+}
