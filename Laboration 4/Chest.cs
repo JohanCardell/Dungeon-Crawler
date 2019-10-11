5df5b79b090602ead32b50a4384389515cd12b1c
Khosro Jadidi <kgosro.jadidi.work@gmail.com>
2019-10-11 14:47:06 +0200

    -Chest:  takes in player  checks for chest type  checks for key  if: has key=> remove key=>inventory++loot  else: erroe message: no key, get one!

--- Laboration 4/Chest.cs
+++ Laboration 4/Chest.cs
@@ -12,32 +12,48 @@ namespace Laboration_4
         public Chest(uint positionX, uint positionY, bool isPassable, bool isVisible, char mapRepresentation, Color color)
             : base(positionX, positionY, isPassable, isVisible, mapRepresentation, color) { }
 
-        public void Interact(Player player)
+        public string Interact(Player player)
         {
             GameSession gameSession = new GameSession();
-            const int treasureValue = 10;
             Random random = new Random();
+            const int treasureValue = 10;
             int diceroll = random.Next(1, 10);
-
-
-
-
-            if (diceroll <= 5)
+            string message = "You don't have a matching key.";
+            foreach (var key in player.inventory.keys)
             {
-                gameSession.Score+=treasureValue;
+                if (key.AssetColor < this.AssetColor)
+                {
+                    message = $"You opened a {this.AssetColor} chest, and a {key.AssetColor} key was removed from your inventory. ";
+                    player.inventory.keys.Remove(key);//use key
+                    if (diceroll <= 5)
+                    {
+                        gameSession.Score += treasureValue;
+                        message += $"Your score increased by {treasureValue}";
+                    }
+                    else if (diceroll >= 6 && diceroll <= 7)
+                    {
+                        player.inventory.potions.Add(new HealthPotion());
+                        message += $"Your got a health potion.";
+                    }
+                    else if (diceroll >= 8 && diceroll <= 9)
+                    {
+                        player.inventory.swords.Add(new Sword(Color.SILVER));
+                        message += $"Your current sword has been updated to a {this.AssetColor} sword.";
+                    }
+                    else
+                    {
+                        player.inventory.swords.Add(new Sword(Color.GOLD));
+                        message += $"Your current sword has been updated to a {this.AssetColor} sword.";
+                    }
+                }
+                break;
             }
-            else if (diceroll >= 6 && diceroll <= 7)
-            {
-                player.inventory.potions.Add(new HealthPotion());
-            }
-            else if (diceroll >= 8 && diceroll <= 9)
-            {
-                player.inventory.swords.Add(new Sword(Color.SILVER));
-            }
-            else
-            {
-                player.inventory.swords.Add(new Sword(Color.GOLD));
-            }
+            return message ;
+        }
+
+        void IInteractable.Interact(Player player)
+        {
+
         }
     }
 }
